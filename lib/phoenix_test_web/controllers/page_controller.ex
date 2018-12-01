3db293affab741f87452e9354a765a3877cbe3f9
jwiddison <jordan.widdison@gmail.com>
2018-12-01 09:42:30 -0800

    Remove bootstrap, generated CSS, etc

--- lib/phoenix_test_web/controllers/page_controller.ex
+++ lib/phoenix_test_web/controllers/page_controller.ex
@@ -4,8 +4,4 @@ defmodule PhoenixTestWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
-
-  def marketing(conn, _params) do
-    render(conn, "marketing.html")
-  end
 end
